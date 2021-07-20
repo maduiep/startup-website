24d989fd32c675b9668292cd9a28ad76a8d0d8f2
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-07-20 09:46:18 +0100

    completed header content and added navbar sectionn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './sass/main.scss'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="navbar__content">
+                    <div className="navbar__left">
+                      <img src="/images/2.png" alt="" />
+                    </div>
+
+                    <ul className="navbar__right">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Showcases</a></li>
+                        <li><a href="/">Portfolio</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
