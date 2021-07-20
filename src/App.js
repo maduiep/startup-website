4e24922a9d7889b3ce59defbf7691e2bd15e6efd
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-07-20 11:41:40 +0100

    added about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './sass/main.scss'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import About from './components/About'
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <About />
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <h4 className="about__h4">
+                            Who we are?
+                        </h4>
+                    </div>
+                    <div className="col-9">
+                        <div className="about__text">
+                            <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos voluptate at, cupiditate id ipsa eligendi dolores tenetur, hic repellat velit rem nesciunt assumenda vero dolor officia illum nisi neque similique?
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora dolores eaque et consequuntur, nulla ducimus repellat. Debitis, hic impedit? Sint odio harum magnam reprehenderit dicta? Error autem quis quam fugiat?
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem earum fuga iure est suscipit hic cum quia, ullam dolores officia sit, corporis provident possimus deleniti reiciendis voluptate praesentium ab nulla.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
