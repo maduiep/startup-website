47eb89da43db656a76029156f1be74ca35cad2e4
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-07-22 00:46:36 +0100

    added showcase section and blank section below it in the design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './sass/main.scss'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import About from './components/About'
+import Showcase from './components/Showcase'
+import Blank from './components/Blank'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Navbar />
       <Header />
       <About />
+      <Showcase />
+      <Blank />
     </>
   );
 }
---
+++ src/components/Showcase.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Showcase = () => {
+    return (
+        <div className="show">
+           <div className="container rel">
+                <div className="show__content">
+                    <h1>So that's us.There's no <br/> other way to put it.</h1>
+                </div>
+
+                <div className="show__card">
+                    <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
+                    <h4 className="show__card-h4">What Client's Say?</h4>
+                    <div className="show__card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nesciunt alias vero natus porro? Quasi eveniet, blanditiis illo minima eius dolorum recusandae sapiente reprehenderit voluptate quo aperiam rem veniam nulla?
+                    </div>
+                </div>
+           </div>
+        </div>
+    )
+}
+
+export default Showcase
